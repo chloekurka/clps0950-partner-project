f5b1546862b92ea52edcfdb15fe47d46d839c57a
taimixu <taimi_xu@brown.edu>
2023-03-15 10:35:38 -0400

    Update startercode.m
    I added a lot of comments to ensure that we know what each variable type is, each function is.
    
    The container map might be useless because bagofngrams might have already created a container map. but once you have the text files then I can try and see if it's useful or not.

--- startercode.m
+++ startercode.m
@@ -6,6 +6,7 @@
 
 directory_name = 'C:\Users\chloe\OneDrive\Desktop\CLPS0950 TXT FILES';
 
+%STEP 1: Load corpus data 
 % List of all text files in the directory
 file_list = dir(fullfile(directory_name, '*.txt'));
 
@@ -18,25 +19,54 @@ for i = 1:length(file_list)
     file_name = fullfile(directory_name, file_list(i).name);
     text = fileread(file_name);
 
+%STEP 2: Preprocess corpus 
     %Links the text to the all_text variable and makes it lowercase
     corpus = [corpus, lower(text)];
 end 
 
 %Removes the punctuation from the corpus 
 corpus = regexprep (corpus, '[^a-zA-Z\s]', ''); 
+%Tokenize document: This means that the raw data is converted into a form
+%that is more easily processed and analysed 
 documents = tokenizedDocument(corpus);
 
 % Split the corpus into individual words. Not sure if this is necessary
 words = strsplit(corpus);
 
-% Generate all possible n-grams
+% STEP 3: Generate n-grams 
+% Generate all possible n-grams of length 2 ('ngrams' variable now has a list of unique n-grams)
+% 'bagOfNgrams' function also has the 'count' property 
+% 'count' property creates a vector of integer counts for each n-gram 
+% 'ngrams' is a cell array where each row represents a single n-gram
 ngrams = bagOfNgrams(documents,'NgramLengths',2);
 
-% Print the n-grams
-disp(ngrams);
+% STEP 4: Count the frequency of n-gram of interest 
+% Create a map to store the n-gram and their counts (Key = ngram, value = count)
+% This container map is currently empty
+ngram_counts = containers.Map; 
 
-% Creates a table of the top 20 bigrams
-topkngrams(ngrams,20,'NGramLengths',2)
+% Loops from first row to the last row of n-grams
+% size(ngrams,1) is used here to refer to the last row 
 
-% Count the number of occurances of one n-gram
-count(corpus, "she is")
+for ii = 1:size(ngrams,1)
+    % Returns the iith row as a cell array
+    ngram = ngrams {ii,:};
+
+    % Check if n-gram is already on the map
+    % isKey() is a function that checks wether a key (ngram) exists in a map container (ngram_counts)
+    if isKey(ngram_counts, ngram)
+        ngram_counts(ngram)=ngram_counts(ngram)+1;
+    else 
+        % If it is not, add it to the map with a count of 1, so the value increases
+        ngram_counts(ngram) = 1; 
+    end 
+end 
+
+%STEP 5: Calculate the probability of each n-gram
+
+%STEP 6: Get the n-gram with the highest probability for the given prefix
+% asking user to write down BLAH
+
+%STEP 7: Display the top most likely word 
+ 
+disp(ngram-counts)
